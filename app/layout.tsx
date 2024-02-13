b76b2d29421ee4ea4278c04bb99b154ad11b390a
ecorey <bengstrom32@gmail.com>
2024-02-13 17:30:24 -0500

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Pixelify_Sans } from "next/font/google"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const pixelify_sans = Pixelify_Sans({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-pixelify_sans", });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className={pixelify_sans.variable}>{children}</body>
     </html>
   );
 }

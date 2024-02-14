7df7e97f31598276de9b199fa9a767eebc47e440
ecorey <bengstrom32@gmail.com>
2024-02-14 11:16:44 -0500

    commit

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,27 +6,22 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import cn from 'classnames'; 
 
-
 const NavBar = () => {
-  // Assuming this is the direct URL to the GIF
-  const gifUrl = "https://media.giphy.com/media/h8DPRPCA7Ol7sDgKNH/giphy.gif";
-
   return (
     <AppBar position="static" sx={{ backgroundColor: '#000', elevation: 0 }}>
       <Toolbar>
-        {/* GIF Image on the far left */}
-        <img src={gifUrl} alt="GIF" style={{ width: 80, height: 80, marginRight: '16px' }} />
-
+        {/* Embed Giphy iframe */}
+        <iframe src="https://giphy.com/embed/jWcQLzQUa6rQc" width="480" height="252" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+        
         <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-        <Typography
-          variant="h1"
-          component="div"
-          className={cn("px-2 py-2 m-2 pixelify_sans")} // Ensure this class is correctly applied
-          sx={{ flexGrow: 1, textAlign: 'center', textShadow: '2px 2px 4px rgba(255, 165, 0, 0.8)' }} 
-        >
-          PREDICTRIX
-        </Typography>
-
+          <Typography
+            variant="h1"
+            component="div"
+            className={cn("px-2 py-2 m-2 pixelify_sans")} 
+            sx={{ flexGrow: 1, textAlign: 'center', textShadow: '2px 2px 4px rgba(255, 165, 0, 0.8)' }} 
+          >
+            PREDICTRIX
+          </Typography>
         </Box>
         <Button variant="outlined" sx={{ border: '1px solid', marginRight: '8px' }}>
           Connect Wallet
